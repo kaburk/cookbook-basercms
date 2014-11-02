c80d42ac0f88b9652e2f05c59e532f3fba90e948
morou <kazuhiro.nakae@gmail.com>
2014-11-02 23:00:44 +0900

    update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'basercms'
 maintainer       'The Independent Rhythm'
 maintainer_email 'kazuhiro.nakae@independentrhythm.com'
-license          'All rights reserved'
+license          'MIT/X11 License'
 description      'installs BaserCMS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
